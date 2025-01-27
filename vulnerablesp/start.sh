d57e9ccb52d69335d03ce564bf5605816cd4f760
alexkutsan <alexkutsan@gmail.com>
2025-01-27 11:35:31 +0400

    remove redundant

--- vulnerablesp/start.sh
+++ vulnerablesp/start.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Replace 127.0.0.1:8000 with SP_HOST in source files
-if [ -z "$SP_HOST" ]; then
-  echo "SP_HOST is not set. Exiting."
-  exit 1
-fi
 
+# Replace 127.0.0.1:8000 with SP_HOST in source files
 if [ -n "$SP_HOST" ]; then
   find /vulnerableSP -type f -exec sed -i "s/127.0.0.1:8000/$SP_HOST/g" {} +
 fi
